b30dbfabe7848f8947e201e0c2e1128768908940
angiejones <jones.angie@gmail.com>
2019-07-03 18:13:29 -0700

    Adding first test

--- webdriver_java/src/test/java/base/BaseTests.java
+++ webdriver_java/src/test/java/base/BaseTests.java
@@ -2,6 +2,8 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import pages.HomePage;
 
 public class BaseTests {
@@ -9,18 +11,17 @@ public class BaseTests {
     private WebDriver driver;
     protected HomePage homePage;
 
+    @BeforeClass
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
         homePage = new HomePage(driver);
-
-        driver.quit();
     }
 
-    public static void main(String args[]){
-        BaseTests test = new BaseTests();
-        test.setUp();
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
     }
 }
---
+++ webdriver_java/src/test/java/login/LoginTests.java
@@ -0,0 +1,22 @@
+package login;
+
+import base.BaseTests;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.SecureAreaPage;
+
+import static org.testng.Assert.*;
+
+public class LoginTests extends BaseTests {
+
+    @Test
+    public void testSuccessfulLogin(){
+        LoginPage loginPage = homePage.clickFormAuthentication();
+        loginPage.setUsername("tomsmith");
+        loginPage.setPassword("SuperSecretPassword!");
+        SecureAreaPage secureAreaPage = loginPage.clickLoginButton();
+        assertTrue(secureAreaPage.getAlertText()
+                .contains("You logged into a secure area!"),
+                "Alert text is incorrect");
+    }
+}
