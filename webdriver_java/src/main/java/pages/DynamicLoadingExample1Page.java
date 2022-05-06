f87e16a1b11b9c3222596dd4f198c070b4ff91ca
this-click <ramona.l.costache@gmail.com>
2022-05-06 16:00:09 +0200

    update deprecated timeout

--- webdriver_java/src/main/java/pages/DynamicLoadingExample1Page.java
+++ webdriver_java/src/main/java/pages/DynamicLoadingExample1Page.java
@@ -22,7 +22,7 @@ public class DynamicLoadingExample1Page {
 
     public void clickStart(){
         driver.findElement(startButton).click();
-        WebDriverWait wait = new WebDriverWait(driver, 5);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         wait.until(ExpectedConditions.invisibilityOf(
                 driver.findElement(loadingIndicator)));
 
--- webdriver_java/src/main/java/pages/DynamicLoadingExample2Page.java
+++ webdriver_java/src/main/java/pages/DynamicLoadingExample2Page.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
+
 public class DynamicLoadingExample2Page {
 
     private WebDriver driver;
@@ -17,7 +19,7 @@ public class DynamicLoadingExample2Page {
 
     public void clickStart() {
         driver.findElement(startButton).click();
-        WebDriverWait wait = new WebDriverWait(driver, 5);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         wait.until(ExpectedConditions.presenceOfElementLocated(loadedText));
     }
 
